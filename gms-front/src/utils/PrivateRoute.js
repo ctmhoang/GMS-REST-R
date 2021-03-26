73d072d8f0ffdea54ff05f02252acb0c994ccd77
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-03-25 22:48:43 -0400

    Fix private route

--- gms-front/src/utils/PrivateRoute.js
+++ gms-front/src/utils/PrivateRoute.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ component, redirectTo, isAuth, path, ...props }) => {
-  if (!isAuth) return <Navigate to={redirectTo} />;
+const PrivateRoute = ({ component, redirectTo, path, ...props }) => {
+  if (!localStorage.getItem("isAuth")) return <Navigate to={redirectTo} />;
 
   return <Route path={path} element={component} {...props} />;
 };
